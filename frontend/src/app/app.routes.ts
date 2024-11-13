f88accfafa296ddacc52947d79df4b886c68adc4
Camila <pereyra.camila@outlook.com>
2024-11-13 01:07:34 -0300

    Inicio front home

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//Agrego el componente Home
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [ 
+    //Agrego el path componente Home
+    { path: 'home', component: HomeComponent } //Defino la ruta del componente
+];

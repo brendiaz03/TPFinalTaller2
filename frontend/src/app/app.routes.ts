0189e352b5f83237b6f753da31d9070ac4686704
Brenda Diaz <brendiaaz03@gmail.com>
2024-11-15 13:08:25 -0300

    division de componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
 //Agrego el componente Home
-import { EditarComponent } from './editar/editar.component';
+import { EditarComponent } from './tareas/editar/editar.component';
+import { TareasComponent } from './tareas/tareas.component';
 
-export const routes: Routes = [ 
-    //Agrego el path componente Home
-    { path: 'editar', component: EditarComponent } //Defino la ruta del componente
+export const routes: Routes = [
+  //Agrego el path componente Home
+  { path: '', component: TareasComponent }, //Defino la ruta del componente
+
+  { path: 'editar', component: EditarComponent }, //Defino la ruta del componente
 ];

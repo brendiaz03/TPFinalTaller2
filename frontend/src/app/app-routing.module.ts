3191a159f48463179946a1566880b71d8a10cde0
Facundo Varela <111824912+facuvarela00@users.noreply.github.com>
2024-11-15 15:31:11 -0300

    Creacion de service
    actualizacion de git ignore y service de To-do

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
  
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/tareas/tareas.component.ts
+++ frontend/src/app/tareas/tareas.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TareasService } from '../core/service/service.service'; 
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tareas',
-  standalone: true,
-  imports: [],
   templateUrl: './tareas.component.html',
-  styleUrl: './tareas.component.css'
+  styleUrls: ['./tareas.component.css']
 })
-export class TareasComponent {
 
+export class TareasComponent implements OnInit {
+  tareas: any[] = [];
+
+  constructor(private tareasService: TareasService) {}
+
+  ngOnInit(): void {
+    this.obtenerTareas();
+  }
+
+  obtenerTareas(): void {
+    this.tareasService.obtenerTareas().subscribe((tareas) => {
+      this.tareas = tareas;
+    });
+  }
+
+  eliminarTarea(id: number): void {
+    this.tareasService.eliminarTarea(id).subscribe(() => {
+      this.obtenerTareas();
+    });
+  }
 }
